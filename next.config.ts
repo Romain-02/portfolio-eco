65f8b8fabca5f1732eb387104a6409c61886f7d2
Romain <romain.hannoir@laposte.net>
2026-03-06 07:37:40 +0100

    fix: rebuild propre

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
+    basePath: '/portfolio-eco' ,
     images: {
         unoptimized: true,
     },
